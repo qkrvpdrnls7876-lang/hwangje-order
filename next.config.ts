04aeb8e42527498c149d033c5869744f36068223
qkrvpdrnls7876 <qkrvpdrnls7876@gmail.com>
2026-05-23 19:20:49 +0900

    fix cloudflare build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-/** next.config.js */
-const nextConfig = {
-  output: 'export', // 이미 배포용 설정
-  async rewrites() {
-    return [
-      {
-        source: '/admin/:path*',   // 기존 middleware 처리하던 경로
-        destination: '/api/proxy/:path*', // 실제 처리할 API route
-      },
-    ]
-  },
-}
+/** @type {import('next').NextConfig} */
 
-module.exports = nextConfig
+const nextConfig = {
+
+};
+
+module.exports = nextConfig;
